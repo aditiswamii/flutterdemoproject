b2eeb6da51da5e661b5542e649f74ade3eb08da6
aditiswamii <aditi.swami@neologicx.com>
2022-01-31 15:57:10 +0530

    mvp

---
+++ lib/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorConstants {
+  //Homepage screen color code
+static const Omnes_font=Color(0xFF707070);
+static const gradient=[Color(0xFFFE4756),Color(0xFFFCA95C),Color(0xFF33E9D6)];
+static const gradient_progress=[Color(0xFFFCA95C),Color(0xFF47B8AC)];
+}
